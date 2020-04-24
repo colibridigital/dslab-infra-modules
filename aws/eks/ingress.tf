8380bacef6526bac7a359cb1f661ce771e94b86e
James Cross <james.cross@colibridigital.io>
2020-04-24 11:29:10 +0100

    remove dup provider

--- aws/eks/ingress.tf
+++ aws/eks/ingress.tf
@@ -1,12 +1,3 @@
-provider helm {
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-    load_config_file       = false
-  }
-}
-
 resource "helm_release" "nginx-ingress" {
   name       = "nginx-ingress"
   repository = data.helm_repository.stable.metadata[0].name
