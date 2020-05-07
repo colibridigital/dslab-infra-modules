32bc0895c86853a886ad044c3e9b3e4054a9d55e
James Cross <james.cross@colibridigital.io>
2020-05-07 16:15:16 +0100

    Add helm provider

--- azure/aks/ingress.tf
+++ azure/aks/ingress.tf
@@ -1,3 +1,18 @@
+provider helm {
+  kubernetes {
+    host                   = "${azurerm_kubernetes_cluster.aks.kube_config.0.host}"
+    client_key             = "${base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)}"
+    client_certificate     = "${base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)}"
+    cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)}"
+  }
+}
+
+
+data "helm_repository" "stable" {
+  name = "stable"
+  url  = "https://kubernetes-charts.storage.googleapis.com"
+}
+
 resource "helm_release" "nginx-ingress" {
   name       = "nginx-ingress"
   repository = data.helm_repository.stable.metadata[0].name
